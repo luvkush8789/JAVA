be48f8b7d85528271a272f166dbbe0abad137885
luvkush <luvkushkumarsharma8789@gmail.com>
2024-01-21 11:50:50 +0530

    update conversion

--- conversion.java
+++ conversion.java
@@ -1,39 +1,39 @@
-import java.util.*;
+// import java.util.*;
 
-// Type Conversion
+// // Type Conversion
 
-public class conversion {
-    public static void main(String args[]) {
-        // conversion of integer to float value
-        // Scanner sc = new Scanner(System.in);
-        // System.out.println("Enter a integer value");
-        // float number = sc.nextInt();
-        // System.out.println("The floating value is : ");
-        // System.out.println(number);
+// public class conversion {
+//     public static void main(String args[]) {
+//         // conversion of integer to float value
+//         // Scanner sc = new Scanner(System.in);
+//         // System.out.println("Enter a integer value");
+//         // float number = sc.nextInt();
+//         // System.out.println("The floating value is : ");
+//         // System.out.println(number);
 
-        // Conversion of integer to long value
+//         // Conversion of integer to long value
 
-        // int a = 44;
-        // long b = a;
-        // System.out.println(b);
+//         // int a = 44;
+//         // long b = a;
+//         // System.out.println(b);
 
-        // Conversion of float value into double value
-        // Scanner sc = new Scanner(System.in);
-        // System.out.println("Enter a float value data");
-        // double number = sc.nextFloat();
-        // System.out.println("The double value is : ");
-        // System.out.println(number);
+//         // Conversion of float value into double value
+//         // Scanner sc = new Scanner(System.in);
+//         // System.out.println("Enter a float value data");
+//         // double number = sc.nextFloat();
+//         // System.out.println("The double value is : ");
+//         // System.out.println(number);
 
-        // Convert Character into integer
+//         // Convert Character into integer
 
-        char ch = 'A';
-        System.out.println("The integer value of character a is : ");
-        int number = ch;
-        System.out.println(number);
+//         char ch = 'A';
+//         System.out.println("The integer value of character a is : ");
+//         int number = ch;
+//         System.out.println(number);
 
-    }
+//     }
 
-}
+// }
 
 // Type Casting
 
@@ -47,3 +47,19 @@ public class conversion {
 // }
 
 // }
+
+// Type Promotion 
+
+import java.util.*;
+
+public class conversion {
+    public static void main(String args[]) {
+        char a = 'a';
+        char b = 'b';
+        System.out.println((int) (b));
+        System.out.println((int) (a));
+        System.out.println(b - a);
+
+    }
+
+}
