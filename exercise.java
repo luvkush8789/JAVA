8606a4c51b66fab4a227fc2f34428e7ef3f5ff49
luvkush <luvkushkumarsharma8789@gmail.com>
2024-01-22 10:43:56 +0530

    exercise updated

--- exercise.java
+++ exercise.java
@@ -31,11 +31,28 @@ public class exercise {
         // square.
         // (Hint : area of a square is (side x side))
 
+        // Scanner sc = new Scanner(System.in);
+        // System.out.println("Enter side of square");
+        // int side = sc.nextInt();
+        // int area = (side) * 2;
+        // System.out.println("The area of square is : " + area);
+
+        // Q:3: Enter cost of 3 items from the user (using float data type) - a pencil,
+        // a pen and
+        // an eraser. You have to output the total cost of the items back to the user as
+        // their bill.
+        // (Add on : You can also try adding 18% gst tax to the items in the bill as an
+        // advanced problem)
+
         Scanner sc = new Scanner(System.in);
-        System.out.println("Enter side of square");
-        int side = sc.nextInt();
-        int area = (side) * 2;
-        System.out.println("The area of square is : " + area);
+        System.out.println("Price of pencil is :");
+        float pencil = sc.nextFloat();
+        System.out.println("Price of pen is :");
+        float pen = sc.nextFloat();
+        System.out.println("Price of eraser is :");
+        float eraser = sc.nextFloat();
+        float total = (pencil + pen + eraser) + ((pencil + pen + eraser) * 0.18f) / 100;
+        System.out.println("Your total price is : " + total);
 
     }
 }
