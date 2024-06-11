68c00301b5995ec49e8603562f9a8234049fe99d
luvkush <luvkushkumarsharma8789@gmail.com>
2024-06-11 07:25:36 +0530

    Arrays file created

---
+++ Arrays.java
@@ -0,0 +1,40 @@
+import java.util.*;
+
+public class Arrays {
+    public static void main(String[] args) {
+
+        // int marks[] = new int[50];
+
+        // int nummbers[] = {1,2,3};
+        // int moreNumbers[] = {4,5,6};
+        // String fruits[] = {"mango" , "orange" , "litchi"};
+
+        int marks[] = new int[100];
+
+        // Scanner sc = new Scanner(System.in);
+        // System.out.print("enter your Physics Marks : ");
+        // marks[0] = sc.nextInt();
+        // System.out.print("enter your Chemistry Marks : ");
+        // marks[1] = sc.nextInt();
+        // System.out.print("enter your Math Marks : ");
+        // marks[2] = sc.nextInt();
+
+        // System.out.println(" phy : " + marks[0]);
+        // System.out.println(" che : " + marks[1]);
+        // System.out.println(" math : " + marks[2]);
+
+        // Update the marks of math
+        // marks[2] = 100;
+        // System.out.println("math : " + marks[2]);
+
+        // Calculate the percentage of all the subjects :
+
+        // int percentage = (marks[0] + marks[1] + marks[2]) / 3;
+        // System.out.println(" percentage is : " + percentage + "%");
+
+        // length of an array
+
+        System.out.println("length of an array is : " + marks.length);
+
+    }
+}
---
+++ InterviewQ.java
@@ -0,0 +1,26 @@
+import java.util.*;
+
+public class InterviewQ {
+
+    public static void runs(int nrr, int overs) {
+        int totalScore = nrr * overs;
+        System.out.println("the total run is : " + totalScore);
+
+    }
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        // System.out.print("Enter Net runrate : ");
+        // int nrr = sc.nextInt();
+        // int overs = 20;
+        // int totalScore = nrr * overs;
+        // System.out.println("The total Score is : " + totalScore);
+
+        // By using function
+        System.out.print("Enter Net runrate : ");
+        int nrr = sc.nextInt();
+        int overs = 20;
+        runs(nrr, overs);
+
+    }
+}
